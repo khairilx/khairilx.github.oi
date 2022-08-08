9eb428a5e8f516e62e2da210847d38c883b98097
khairilx <irilnst23@gmail.com>
2022-08-08 17:04:46 +0700

    first commit

---
+++ application/views/types_form.php
@@ -0,0 +1,29 @@
+<!-- Main content -->
+<section class='content'>
+  <div class='row'>
+    <div class='col-xs-12'>
+      <div class='box'>
+        <div class='box-header'>
+
+          <h3 class='box-title'>TYPE</h3>
+          <div class='box box-primary'>
+            <form action="<?php echo $action; ?>" method="post">
+              <table class='table table-bordered'>
+                <tr>
+                  <td>Metode <?php echo form_error('type') ?></td>
+                  <td><input type="text" class="form-control" name="type" id="type" placeholder="Type Metode" value="<?php echo $type; ?>" />
+                  </td>
+                  <input type="hidden" name="id" value="<?php echo $id; ?>" />
+                <tr>
+                  <td colspan='2'><button type="submit" class="btn btn-primary"><?php echo $button ?></button>
+                    <a href="<?php echo site_url('types') ?>" class="btn btn-default">Cancel</a>
+                  </td>
+                </tr>
+
+              </table>
+            </form>
+          </div><!-- /.box-body -->
+        </div><!-- /.box -->
+      </div><!-- /.col -->
+    </div><!-- /.row -->
+</section><!-- /.content -->
